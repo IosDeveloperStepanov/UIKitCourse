0cea87d59eeb36d25e650bc4f85a8d6e2666f49e
igor81258 <serry07@rambler.ru>
2024-08-13 15:29:21 +0300

    Initial Commit

---
+++ UIKitCourse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitCourse
+//
+//  Created by Игорь Степанов on 13.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
